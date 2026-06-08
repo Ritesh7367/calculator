6f5e8268d45e5b78512d395b6b763c19824158bb
ritesh kumar <riteshssc452@gmail.com>
2026-06-08 22:11:06 +0530

    basic calculator

---
+++ script.js
@@ -0,0 +1,19 @@
+let display = document.getElementById('display');
+let buttons = document.querySelectorAll('button');
+let arr=Array.from(buttons);
+arr.forEach(button =>{
+    button.addEventListener('click', (e)=>{
+        if(e.target.innerText == '='){
+            display.value = eval(display.value);
+        }
+        else if(e.target.innerText == 'C'){
+            display.value = '';
+        }  
+        else if(e.target.innerText == 'BACK'){ 
+            display.value = display.value.slice(0, -1);
+        }
+        else{
+            display.value += e.target.innerText;
+        }
+    })
+})
